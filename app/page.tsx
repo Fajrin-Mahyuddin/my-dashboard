18e823eee7548e4981a1b6725cec99ebd7f5d168
fajrin <fajrin.telkom12@gmail.com>
2024-05-29 16:55:37 +0800

    feature/dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,12 @@ import SkeletonTable from "components/organisms/skeleton/Tabel";
 import SkeletonCard from "components/organisms/skeleton/Card";
 import BaseLayout from "components/templates/Layout";
 import { useEffect, useState } from "react";
+import CardStatistic from "components/organisms/card/CardStatistic";
+import {
+  EnvelopeIcon,
+  PresentationChartBarIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/24/outline";
 
 export default function Page({
   params,
@@ -24,11 +30,33 @@ export default function Page({
   return (
     <BaseLayout>
       <div className="flex flex-col bg-[#fff] h-full p-4 rounded-[10px]">
-        <div className="flex gap-4">
-          <SkeletonCard isLoading={isLoading} />
-          <SkeletonCard isLoading={isLoading} />
-          <SkeletonCard isLoading={isLoading} />
-        </div>
+        {isLoading ? (
+          <div className="flex gap-4">
+            <SkeletonCard isLoading={isLoading} />
+            <SkeletonCard isLoading={isLoading} />
+            <SkeletonCard isLoading={isLoading} />
+          </div>
+        ) : (
+          <div className="flex gap-2">
+            <CardStatistic
+              label="Messages"
+              desc="Lorem ipsum dolor sit amet consectetur"
+              icon={EnvelopeIcon}
+            />
+            <CardStatistic
+              label="Inbox"
+              desc="Lorem ipsum dolor sit amet"
+              icon={PresentationChartBarIcon}
+              colors="#F26440"
+            />
+            <CardStatistic
+              label="Product sell"
+              desc="Lorem ipsum "
+              icon={ShoppingCartIcon}
+              colors="#E6AB35"
+            />
+          </div>
+        )}
 
         <table className="border-collapse border-spacing-6 mt-10 table-auto w-full">
           <thead>
@@ -40,7 +68,7 @@ export default function Page({
           </thead>
           <tbody>
             {isLoading ? (
-              <SkeletonTable isLoading={isLoading} />
+              <SkeletonTable isLoading={isLoading} rows={3} cols={2} />
             ) : (
               <>
                 <tr className="border-b-[1px] border-b-[#dfdfdf]">
@@ -49,6 +77,23 @@ export default function Page({
                   </td>
                   <td>Malcolm Lockyer</td>
                   <td>1961</td>
+                </tr>
+                <tr className="border-b-[1px] border-b-[#dfdfdf]">
+                  <td className="py-4 pl-2">Witchy Woman</td>
+                  <td>The Eagles</td>
+                  <td>1972</td>
+                </tr>
+                <tr className="border-b-[1px] border-b-[#dfdfdf]">
+                  <td className="py-4 pl-2">
+                    The Sliding Mr. Bones (Next Stop, Pottersville)
+                  </td>
+                  <td>Malcolm Lockyer</td>
+                  <td>1961</td>
+                </tr>
+                <tr className="border-b-[1px] border-b-[#dfdfdf]">
+                  <td className="py-4 pl-2">Witchy Woman</td>
+                  <td>The Eagles</td>
+                  <td>1972</td>
                 </tr>
                 <tr className="border-b-[1px] border-b-[#dfdfdf]">
                   <td className="py-4 pl-2">Witchy Woman</td>
--- components/organisms/skeleton/Card.tsx
+++ components/organisms/skeleton/Card.tsx
@@ -5,13 +5,11 @@ const SkeletonCard = ({ isLoading }: { isLoading: boolean }) => {
     return null;
   }
   return (
-    <div className="border shadow rounded-md p-4  w-full mx-auto">
-      <div className="animate-pulse flex space-x-4">
+    <div className="border shadow rounded-md p-4 w-full mx-auto">
+      <div className="animate-pulse gap-4 flex flex-col items-end">
         <div className="rounded-full bg-slate-700 h-10 w-10"></div>
-        <div className="flex-1 space-y-6 py-1">
-          <div className="h-2 bg-slate-700 rounded"></div>
-          <div className="h-2 bg-slate-700 rounded"></div>
-        </div>
+        <div className="h-2 w-full bg-slate-700 rounded "></div>
+        <div className="h-2 w-full bg-slate-700 rounded "></div>
       </div>
     </div>
   );
